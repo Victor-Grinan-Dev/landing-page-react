8f2caaf1aa277cd6a1bbb11e14aa9f1bcbeac26c
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-06-20 10:20:49 +0300

    starter file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="global-container flex items-center justify-center min-h-screen bg-rose-50">
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
